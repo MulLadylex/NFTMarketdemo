bd5f1267a590897afc1f131e44082f9e3a32930c
Ladylex <116450914+MulLadylex@users.noreply.github.com>
2023-12-06 22:38:57 +0800

    upgrade

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,7 +1,13 @@
 /** @type import('hardhat/config').HardhatUserConfig */
 require("@nomiclabs/hardhat-waffle");
 require("solidity-coverage");
+require('hardhat-abi-exporter');
 
 module.exports = {
   solidity: "0.8.20",
+  abiExporter: {
+    path: './abi',
+    clear: true,
+    flat: true,
+  },
 };
