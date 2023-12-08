a07fa42df72153b2ddeeaf1511f4644e5f67c400
Ladylex <116450914+MulLadylex@users.noreply.github.com>
2023-12-08 14:31:43 +0800

    Update erc721_nft.sol

--- contracts/erc721_nft.sol
+++ contracts/erc721_nft.sol
@@ -3,23 +3,21 @@ pragma solidity ^0.8.20;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract NFTM is ERC721, ERC721Enumerable, Ownable {
+contract NFTM is ERC721, ERC721Enumerable, ERC721URIStorage, Ownable {
     uint256 private _nextTokenId;
 
-    constructor(address initialOwner)
+    constructor()
         ERC721("NFTM", "NFTM")
-        Ownable(initialOwner)
+        Ownable(msg.sender)
     {}
 
-    function _baseURI() internal pure override returns (string memory) {
-        return "https://xiangmingda.com/";
-    }
-
-    function safeMint(address to) public onlyOwner {
+    function safeMint(address to, string memory uri) public onlyOwner {
         uint256 tokenId = _nextTokenId++;
         _safeMint(to, tokenId);
+        _setTokenURI(tokenId, uri);
     }
 
     // The following functions are overrides required by Solidity.
@@ -39,10 +37,17 @@ contract NFTM is ERC721, ERC721Enumerable, Ownable {
         super._increaseBalance(account, value);
     }
 
+    function tokenURI(uint256 tokenId) 
+        public 
+        view 
+        override (ERC721, ERC721URIStorage) returns (string memory) {
+            return super.tokenURI(tokenId);
+        }
+
     function supportsInterface(bytes4 interfaceId)
         public
         view
-        override(ERC721, ERC721Enumerable)
+        override(ERC721, ERC721Enumerable, ERC721URIStorage)
         returns (bool)
     {
         return super.supportsInterface(interfaceId);
